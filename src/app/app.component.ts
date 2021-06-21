b0e524d255d5b36bb600f5cdb038017e9ab6413b
BlackAkr <BlackAkr@gmail.com>
2021-06-21 15:26:00 +0300

    add basic functionality

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,22 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'calculator';
+  answer: string = "0"
+  displayId(id:string)
+  {
+    console.log(id);
+  }
+  clickedValueShow(value:any)
+  {
+    if(this.answer == "0")
+    {
+      this.answer = value
+    }
+    else{
+      this.answer+=value;
+    }
+  }
+  array_of_symbols: string[][] = [
+    ["7","8", "9", "/"], ["4", "5", "6", "*"], ["1","2","3","-"], ["0",".","%","+"]
+  ]
 }
